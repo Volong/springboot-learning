b861344f427a90aa13f9a35ddcf3f9150faaf3f8
volong <yhlona523@gmail.com>
2019-03-28 18:01:30 +0800

    thymeleaf

---
+++ src/main/java/github/io/volong/controller/HelloController.java
@@ -0,0 +1,25 @@
+package github.io.volong.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index(ModelMap map) {
+        map.addAttribute("message", "http://www.yihailong.top");
+        return "hello";
+    }
+
+    @RequestMapping("/index")
+    public String index() {
+        return "index";
+    }
+
+    @RequestMapping("/home")
+    public String home() {
+        return "home";
+    }
+}
