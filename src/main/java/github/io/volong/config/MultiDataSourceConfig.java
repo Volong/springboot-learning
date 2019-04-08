d25dd2daddc7741d8d05d2a8e0972fb14b7521c4
volong <yhlona523@gmail.com>
2019-04-08 09:22:29 +0800

    :smile:mybatis config with druids

---
+++ src/main/java/github/io/volong/config/MultiDataSourceConfig.java
@@ -0,0 +1,29 @@
+package github.io.volong.config;
+
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
+
+/**
+ * @time 2019-04-08
+ */
+@Configuration
+public class MultiDataSourceConfig {
+
+    @Primary
+    @Bean("oneDataSource")
+    @ConfigurationProperties("spring.datasource.druid.one")
+    public DataSource dataSourceOne() {
+        return DruidDataSourceBuilder.create().build();
+    }
+
+    @Bean(name = "twoDataSource")
+    @ConfigurationProperties("spring.datasource.druid.two")
+    public DataSource dataSourceTwo(){
+        return DruidDataSourceBuilder.create().build();
+    }
+}
---
+++ src/main/java/github/io/volong/mapper/two/UserTwoMapper.java
@@ -0,0 +1,26 @@
+package github.io.volong.mapper.two;
+
+import github.io.volong.entity.UserEntity;
+import github.io.volong.param.UserParam;
+
+import java.util.List;
+
+/**
+ * 方法名需要与 UserMapper.xml 中的 id 名一致
+ */
+public interface UserTwoMapper {
+
+    List<UserEntity> getAll();
+
+    UserEntity getOne(Long id);
+
+    void insert(UserEntity user);
+
+    void update(UserEntity user);
+
+    void delete(Long id);
+
+    List<UserEntity> getList(UserParam userParam);
+
+    int getCount(UserParam userParam);
+}
