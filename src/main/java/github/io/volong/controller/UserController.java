a4c9d4430261dd11765dc788a587ef152d7275d3
Volong <yhlona523@gmail.com>
2019-04-09 23:39:54 +0800

    :smile:sprint cache

--- src/main/java/github/io/volong/controller/UserController.java
+++ src/main/java/github/io/volong/controller/UserController.java
@@ -2,14 +2,22 @@ package github.io.volong.controller;/**
  * @time 2019-03-29
  */
 
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
- *
  * @time 2019-03-29
  */
 
-@Controller
+@RestController
 public class UserController {
 
+    @RequestMapping("/hello")
+    @Cacheable(value = "helloCache")
+    public String hello(String name) {
+        System.out.println("没有走缓存！");
+        return "hello " + name;
+    }
 }
